8bfcf31197d2f056a45675d48e62be46a6efbaec
Anna Toll <anna.toll@protonmail.com>
2023-10-09 09:52:39 +0200

    Added cors, and added frontend to allowed origins

--- app-express/src/server/app.js
+++ app-express/src/server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const api_endpoints = require('../api');
 const app = express();
+
+const corsOrigin = [
+  'http://localhost:3000',
+];
+
+app.use(cors({ 
+  origin: corsOrigin
+}));
 
 app.use(bodyParser.json());
 
