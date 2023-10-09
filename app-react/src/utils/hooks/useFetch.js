490e884056e2a3acff9df6ebdb316794869e6130
Anna Toll <anna.toll@protonmail.com>
2023-10-09 09:57:37 +0200

    Created custom hook to handle fetch requests

---
+++ app-react/src/utils/hooks/useFetch.js
@@ -0,0 +1,63 @@
+import { useState } from "react"
+import useTransactions from "../../state/transactions/useTransactions"
+
+const useFetch = (resInit) => {
+
+    const { toDispatch } = useTransactions()
+    const [response, setResponse] = useState(resInit)
+    const [error, setError] = useState('')
+
+    const fetchBase = async (url, method = 'GET', dataObj = {}, actionOnOk = null) => {
+
+        setError('')
+        setResponse(resInit)
+        const requestObject = { method }
+
+        if (method !== 'GET') {
+
+            requestObject.body = JSON.stringify(dataObj)
+            requestObject.headers = {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}${url}`, requestObject)
+            const data = await res.json()
+
+            if (!res.ok) {
+
+                setError(data.error)
+                setResponse(data)
+
+            } else {
+
+                setError('')
+                setResponse(data)
+
+                if (actionOnOk) {
+
+                    toDispatch({
+                        type: actionOnOk,
+                        data
+                    })
+                }
+            }
+
+        } catch (error) {
+            setError('Oops! Something unexpected went wrong.')
+        }
+    }
+
+    const fetchGet = (url, actionOnOk = null) => {
+        fetchBase(url, 'GET', {}, actionOnOk)   
+    }
+
+    const fetchPost = (url, dataObj, actionOnOk = null) => {
+        fetchBase(url, 'POST', dataObj, actionOnOk)   
+    }
+
+    return { response, error, fetchGet, fetchPost }
+}
+
+export default useFetch
